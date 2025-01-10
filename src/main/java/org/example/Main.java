1e16cd5ca27c5f9d548f97cc33ca2fb34e89563e
MaxRejnefelt <Maxrejnefelt@gmail.com>
2025-01-10 10:22:57 +0100

    Updated Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello Github Actions");
+        System.out.println("Im a quick learner");
     }
 }
